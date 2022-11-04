6e993b4faaf2555380920acd1e411f3dc4455ad1
pratham-wolife <pratham.kumar@wo.life>
2022-11-04 16:36:00 +0530

    style faqs

--- src/components/sections/faq.js
+++ src/components/sections/faq.js
@@ -3,23 +3,23 @@ import styled from 'styled-components';
 import { Section } from '../global';
 
 const Faq = () => {
-	const questionText = (ques1, ques2, ans1, ans2, showHorizonalRule) => {
+	const QuestionContainer = (ques1, ques2, ans1, ans2, showHorizontalRule) => {
 		return (
-			<>
-				<h2>
+			<div>
+				<Question>
 					{ques1}
 					<br />
 					{ques2}
-				</h2>
+				</Question>
 				{ans1 !== '' && (
-					<p>
+					<Answer>
 						{ans1}
 						<br />
 						{ans2}
-					</p>
+					</Answer>
 				)}
-				{showHorizonalRule && <HorizontalRule />}
-			</>
+				{showHorizontalRule && <HorizontalRule />}
+			</div>
 		);
 	};
 
@@ -31,15 +31,15 @@ const Faq = () => {
 				</ImageWrapper>
 				<TextWrapper>
 					<PrimaryHeading>FAQs</PrimaryHeading>
-					{questionText(
+					{QuestionContainer(
 						'Does my Earning depend on the number of',
 						'followers I have?',
 						'No, You can earn irrespective of the number of',
 						'followers you have.',
 						true,
 					)}
-					{questionText('Does my Earning depend on the number of', 'followers I have?', '', '', true)}
-					{questionText('Does my Earning depend on the number of', 'followers I have?', '', '', false)}
+					{QuestionContainer('Does my Earning depend on the number of', 'followers I have?', '', '', true)}
+					{QuestionContainer('Does my Earning depend on the number of', 'followers I have?', '', '', false)}
 				</TextWrapper>
 			</CenterDiv>
 		</Section>
@@ -50,9 +50,21 @@ const CenterDiv = styled.div`
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
-	padding: 4rem 0;
+	color: ${(props) => props.theme.color.background.white};
 	background-color: ${(props) => props.theme.color.background.regular};
-	border: 1px solid red;
+`;
+
+const ImageWrapper = styled.div`
+	width: 50%;
+	padding: 1rem;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+const PrimaryHeading = styled.h1`
+	font-size: 4rem;
+	font-weight: bold;
 `;
 
 const TextWrapper = styled.div`
@@ -62,32 +74,23 @@ const TextWrapper = styled.div`
 	flex-direction: column;
 	justify-content: center;
 	align-items: center;
-	color: ${(props) => props.theme.color.background.white};
-	h2 {
-		font-size: 2rem;
-	}
-	p {
-		font-size: 1.5rem;
-		font-weight: 500;
-	}
 `;
 
-const PrimaryHeading = styled.h1`
-	font-size: 3rem;
-	color: ${(props) => props.theme.color.background.white};
+const Question = styled.h2`
+	font-size: 2rem;
+	letter-spacing: 0.1px;
+	line-height: 1.5;
 `;
 
-const ImageWrapper = styled.div`
-	width: 50%;
-	padding: 1rem;
-	border: 1px solid blue;
-	display: flex;
-	justify-content: center;
-	align-items: center;
+const Answer = styled.p`
+	padding: 1rem 0;
+	font-size: 1.5rem;
+	letter-spacing: 0.1px;
 `;
 
 const HorizontalRule = styled.hr`
-	width: 70%;
+	margin: 0;
+	width: 100%;
 	height: 2px;
 	background-color: ${(props) => props.theme.color.background.white};
 `;
