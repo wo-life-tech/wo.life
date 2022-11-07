0e4127e84b578622ed7c23c31c343b56952078de
pratham-wolife <pratham.kumar@wo.life>
2022-11-07 08:37:37 +0530

    using reusable component in faq

--- src/components/sections/faq.js
+++ src/components/sections/faq.js
@@ -2,8 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 import { Section } from '../global';
 import CenterDiv from '../common/layout/CenterDiv';
-import { HorizontalRow } from '../common/layout/Text';
+import { HorizontalRow, PrimaryHeading } from '../common/layout/Text';
 import Image from './Image';
+import { TextWrapper } from '../common/layout/TextWrapper';
 
 const Faq = () => {
 	const QuestionContainer = (ques1, ques2, ans1, ans2, showHorizontalRow) => {
@@ -31,7 +32,7 @@ const Faq = () => {
 			<CenterDiv isPaddingRequired={false}>
 				<Image src="/faq.png" altText="faq" maxWidth="100%" />
 				<TextWrapper>
-					<PrimaryHeading>FAQs</PrimaryHeading>
+					<PrimaryHeading heading="FAQs" isLarger={true} />
 					{QuestionContainer(
 						'Does my Earning depend on the number of',
 						'followers I have?',
@@ -46,20 +47,6 @@ const Faq = () => {
 		</Section>
 	);
 };
-
-const PrimaryHeading = styled.h1`
-	font-size: 4rem;
-	font-weight: bold;
-`;
-
-const TextWrapper = styled.div`
-	width: 60%;
-	padding: 1rem;
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-`;
 
 const Question = styled.h2`
 	font-size: 2rem;
