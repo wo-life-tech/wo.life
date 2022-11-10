d7b280c695dec701d76327d44bd575068b4ec029
pratham-wolife <pratham.kumar@wo.life>
2022-11-10 16:47:17 +0530

    basic features styling desktop, mobile

--- src/components/sections/features.js
+++ src/components/sections/features.js
@@ -7,84 +7,89 @@ import ImgWithFallBack from '../common/ImgWithFallBack';
 const Features = () => {
 	const Card = (heading, src, fallbackSrc, altText, text) => {
 		return (
-			<CenterDiv>
-				<SecText>{heading}</SecText>
-				<Text>{text}</Text>
+			<CardContainer>
+				<CardHeading>{heading}</CardHeading>
+				<CardText>{text}</CardText>
 				<ImgWithFallBack
 					src={src}
 					fallbackSrc={fallbackSrc}
 					altText={altText}
 					style={{ maxWidth: '90%', paddingTop: '2.5rem' }}
 				/>
-			</CenterDiv>
+			</CardContainer>
 		);
 	};
 
 	return (
 		<Section id="features">
-			<CenterDiv>
-				<Heading heading="Features" />
-				<ContentWrapper>
-					{Card(
-						'Socializing',
-						'/socializing.webp',
-						'/socializing.png',
-						'socializing',
-						'Engage and get entertained through Posts and short videos',
-					)}
-					{Card(
-						'Shopping',
-						'/smartphone-shopping.webp',
-						'/smartphone-shopping.png',
-						'shop-via-app',
-						'Shop from your favourite brands through shoppable content & marketplace ',
-					)}
-					{Card(
-						'Earning',
-						'/influencer-earning.webp',
-						'/influencer-earning.png',
-						'influencer-earning',
-						'Track all your earnings in the    wallet. Earn from a highly robust affiliate',
-					)}
-				</ContentWrapper>
-			</CenterDiv>
+			<Heading heading="Features" />
+			<Container>
+				{Card(
+					'Socializing',
+					'/socializing.webp',
+					'/socializing.png',
+					'socializing',
+					'Engage and get entertained through Posts and short videos',
+				)}
+				{Card(
+					'Shopping',
+					'/smartphone-shopping.webp',
+					'/smartphone-shopping.png',
+					'shop-via-app',
+					'Shop from your favourite brands through shoppable content & marketplace',
+				)}
+				{Card(
+					'Earning',
+					'/influencer-earning.webp',
+					'/influencer-earning.png',
+					'influencer-earning',
+					'Track all your earnings in the wallet. Earn from a highly robust affiliate',
+				)}
+			</Container>
 		</Section>
 	);
 };
 
-const CenterDiv = styled.div`
+const Container = styled.div`
+	margin: 0;
+	padding: 0 3rem;
 	display: flex;
-	justify-content: flex-start;
-	align-items: center;
-	flex-direction: column;
-	gap: 0.5rem;
+	justify-content: space-between;
+	align-items: space-between;
+	border: 2px solid grey;
 	color: ${(props) => props.theme.color.background.white};
 	background-color: ${(props) => props.theme.color.background.regular};
-	margin: 0;
-	padding: 0;
-`;
-
-const ContentWrapper = styled.div`
-	width: 100%;
-	display: flex;
-	gap: 2rem;
-	align-items: center;
-	justify-content: space-around;
 	@media (max-width: ${(props) => props.theme.screen.md}) {
 		flex-direction: column;
 	}
 `;
 
-const SecText = styled.h2`
+const CardContainer = styled.div`
+	margin: 0;
+	padding: 0 !important;
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	align-items: center;
+	border: 1px solid white;
+	@media (max-width: ${(props) => props.theme.screen.md}) {
+		padding: 1rem;
+	}
+`;
+
+const CardHeading = styled.h2`
 	margin: 1rem 0;
 	padding: 10px 0;
 	letter-spacing: 0.1rem;
+	border: 1px solid blue;
 `;
 
-const Text = styled.p`
+const CardText = styled.p`
 	padding: 0 1rem;
 	font-size: 1.5rem;
 	line-height: 2rem;
+	border: 1px solid yellow;
 `;
 
 export default Features;
