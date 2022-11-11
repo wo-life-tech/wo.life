2f29ca4a59b623b8344fff43e442341c4fc098a3
pratham-wolife <pratham.kumar@wo.life>
2022-11-11 11:53:38 +0530

    add new faq section

---
+++ src/components/FaqData.js
@@ -0,0 +1,18 @@
+const FaqData = [
+	{
+		id: 1,
+		title: 'How much money can i make on WO & can i withdraw it?',
+		info: 'You can earn upto 20% of the sale conversion through your social influence. Yes, you can withdraw your earnings or use the same to buy new products.',
+	},
+	{
+		id: 2,
+		title: 'Does my Earning depend on the number of followers I have?',
+		info: 'No, You can earn irrespective of the number of followers you have. You can leverage your other social media channels to earn as well.',
+	},
+	{
+		id: 3,
+		title: 'Can I use the earned money to buy other products on wo ?',
+		info: 'Yes. 1 coin = 1 rupee. You can buy anything on our platform with the money you have earned.',
+	},
+];
+export default FaqData;
--- src/components/sections/faq.js
+++ src/components/sections/faq.js
@@ -1,32 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Section } from '../global';
+import { HeadingText } from '../common/layout/Text';
 import CenterDiv from '../common/layout/CenterDiv';
-import { HorizontalRow, PrimaryHeading } from '../common/layout/Text';
-import { TextWrapper } from '../common/layout/TextWrapper';
 import Image from '../common/image';
+import './faq.css';
+import FaqData from '../FaqData';
+import SingleQuestion from './SingleQuestion';
 
 const Faq = () => {
-	const QuestionContainer = (question, answer, showHorizontalRow) => {
-		return (
-			<div>
-				<Question>{question}</Question>
-				{answer !== '' && <Answer>{answer}</Answer>}
-				{showHorizontalRow && <HorizontalRow />}
-			</div>
-		);
-	};
+	const [questions, setQuestions] = useState(FaqData);
 
 	return (
 		<Section id="faqs">
-			<CenterDiv isPaddingRequired={true} isflexDirectionReverse={true}>
+			<CenterDiv isPaddingRequired={false} isflexDirectionReverse={true}>
 				<Image
 					src="/faq.webp"
 					fallbackSrc="/faq.png"
 					altText="faq"
 					style={{ maxWidth: '80%', padding: '1rem' }}
 				/>
-				<FAQsContainer></FAQsContainer>
+				<FAQsContainer>
+					<HeaderBox>
+						<HeadingText heading1="FAQs" />
+					</HeaderBox>
+					<div className="container">
+						<section className="info">
+							{questions.map((question) => (
+								<SingleQuestion key={question.id} {...question} />
+							))}
+						</section>
+					</div>
+				</FAQsContainer>
 			</CenterDiv>
 		</Section>
 	);
@@ -35,18 +40,21 @@ const Faq = () => {
 const FAQsContainer = styled.div`
 	margin: 0;
 	padding: 0;
-	border: 1px solid blue;
+	width: 50%;
+	color: ${(props) => props.theme.color.background.white};
+	background-color: ${(props) => props.theme.color.background.regular};
+	@media (max-width: ${(props) => props.theme.screen.md}) {
+		width: 100%;
+	}
 `;
 
-const Question = styled.h4`
-	letter-spacing: 0.1px;
-	line-height: 1.15;
-`;
-
-const Answer = styled.p`
-	padding: 1rem 0;
-	letter-spacing: 0.1px;
-	line-height: 1.15;
+const HeaderBox = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	@media (max-width: ${(props) => props.theme.screen.md}) {
+		padding-left: 5rem;
+	}
 `;
 
 export default Faq;
